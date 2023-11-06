22812692f735cf0a9c4c8dd8c4c1dd2bbf0fdb97
Valentynszn <valentineogbuagu86@gmail.com>
2023-11-06 03:20:09 -0800

    updated

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,19 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - validate the code
+ *
+ * Return: Always returns 0.
+ */
+
+int main(void)
+
+{
+    int s;
+
+    s = 98 + SIZE;
+    printf("%d\n", s);
+    return (0);
+}
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - writes the name of the file
+ *
+ * Return: Always returns 0 (Success)
+ */
+
+int main(void)
+
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
